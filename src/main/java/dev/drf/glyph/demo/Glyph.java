11f9e560533af886ec99f134786cd718c49ffb4f
drfdev <drafff@gmail.com>
2020-01-08 16:05:13 +0300

    add main functions

--- src/main/java/dev/drf/glyph/demo/Glyph.java
+++ src/main/java/dev/drf/glyph/demo/Glyph.java
@@ -1,4 +1,33 @@
 package dev.drf.glyph.demo;
 
 public interface Glyph extends CharSequence {
+    /*
+    CharSequence:
+    public int length();
+    public char charAt(int index);
+    public CharSequence subSequence(int start, int end);
+
+    Object:
+    public String toString();
+	public boolean equals();
+	public int hashCode();
+     */
+
+    void build(StringBuilder builder);
+    Glyph reverse();
+    Glyph toUpperCase();
+    Glyph toLowerCase();
+    Glyph subGlyph(int start, int end);
+
+    @Override
+    default CharSequence subSequence(int start, int end) {
+        return subGlyph(start, end);
+    }
+
+    @Override
+    default String toString() {
+        StringBuilder builder = new StringBuilder();
+        build(builder);
+        return builder.toString();
+    }
 }
--- src/main/java/dev/drf/glyph/demo/group/GroupGlyph.java
+++ src/main/java/dev/drf/glyph/demo/group/GroupGlyph.java
@@ -1,6 +1,10 @@
 package dev.drf.glyph.demo.group;
 
 import dev.drf.glyph.demo.AbstractGlyph;
+import dev.drf.glyph.demo.Glyph;
 
-public abstract class GroupGlyph extends AbstractGlyph {
+public abstract class GroupGlyph extends AbstractGlyph implements Iterable<Glyph> {
+    /*
+    public Iterator<Glyph> iterator();
+     */
 }
